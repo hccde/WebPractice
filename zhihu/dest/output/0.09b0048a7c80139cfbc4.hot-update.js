16664b388fee250588014c097ac5b050c5e5f054
hccde <1076663958@qq.com>
2016-07-18 15:07:20 +0800

    save

---
+++ zhihu/dest/output/0.09b0048a7c80139cfbc4.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate(0,{
+
+/***/ 13:
+/***/ function(module, exports) {
+
+	"use strict";
+
+	module.exports = {
+		data: function data() {
+			return {
+				navitems: [{ name: "首页" }, { name: "首页2" }]
+			};
+		}
+	};
+
+/***/ }
+
+})
---
+++ zhihu/dest/output/0.decc58316a622ac6eb7a.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate(0,{
+
+/***/ 14:
+/***/ function(module, exports) {
+
+	module.exports = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n<div class=\"nav-wrap\">\n\t<div class=\"nav-logo\">\n\t\t\n\t</div>\n\t<div class=\"nav-search-wrap\">\n\t\t<input type=\"type\" name=\"search\" required=\"\" placeholder=\"搜索你感兴趣的内容...\">\n\t\t<div>\n\t\t</div>\n\t</div>\n\t<ul class=\"nav-list\">\n\t\t<li v-for=\"item in navitems\">\n\t\t\t{{item.name}}\n\t\t</li>\n\t</ul>\n\t<input type=\"button\" name=\"ask\" value=\"提问\">\n\t<div class=\"nav-person\">\n\t\t<div>\n\t\t\t\n\t\t</div>\n\t\t<span>\n\t\t\tvgyyuiof\n\t\t</span>\n\t</div>\n</div>\n";
+
+/***/ }
+
+})
